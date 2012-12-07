820440df63326dc2e6683779395ceed46bb4cf83
Taira <taira@Taira-N5030.(none)>
2012-12-07 11:27:58 +0100

    Second commit :)

--- project/src/main/java/diffusion/App.java
+++ project/src/main/java/diffusion/App.java
@@ -1,11 +1,17 @@
 package diffusion;
 
+import diffusion.display.Display;
+import diffusion.display.DisplayImpl;
+import diffusion.sensor.Sensor;
+import diffusion.sensor.SensorImpl;
+
 public class App {
 	public static void main(String[] args) {
+		Sensor sensor = new SensorImpl();
+		Display display = new DisplayImpl();
 		
 		
 		
-		
-//		window.registerObserver((ObserverWheel) controler);
+		sensor.attach(display);
 	}
 }
