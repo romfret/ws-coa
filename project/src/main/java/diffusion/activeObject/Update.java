4e50160b0b4915794aba669cf814646c2f6f573c
Taira <taira@Taira-N5030.(none)>
2012-12-11 16:58:23 +0100

    update class of active object pattern

--- project/src/main/java/diffusion/activeObject/Update.java
+++ project/src/main/java/diffusion/activeObject/Update.java
@@ -11,14 +11,22 @@ public class Update implements Callable<Object> {
 	private Display display;
 	
 	public Object call() throws Exception {
+		display.update(sensor);
 		return null;
 	}
 	
 	/**
-	 * 
+	 * Set the observer of type Dislay
 	 */
 	public void setObserver(Display display) {
 		this.display = display;
+	}
+	
+	/**
+	 * Set the subject of type Sensor
+	 */
+	public void setSubject(Sensor sensor) {
+		this.sensor = sensor;
 	}
 
 }
