727934d157bbf84cc252db03bb32da25e36de7b1
Taira <taira@Taira-N5030.(none)>
2012-12-14 01:20:31 +0100

    Infos sur strategie. Cf. AtomicDiffusion.java

--- project/src/main/java/diffusion/sensor/strategy/AtomicDiffusion.java
+++ project/src/main/java/diffusion/sensor/strategy/AtomicDiffusion.java
@@ -18,6 +18,19 @@ public class AtomicDiffusion implements IDiffusion {
 	public void configure() {
 
 	}
+	
+	/**
+	 * TODO :
+	 * Voir PC Strategy sur developpez.com. PC ayant pour but de faire varier une partie de l'algo du context suivant une strategie.
+	 * => C'est lors de l'instanciation du context (Sensor) qu'une strategie lui est passee en parametre.
+	 * C'est le context qui lancera la strategie :
+	 * 
+	 * // Dans Sensor 
+	 * public void operation() {
+	 * 		strategie.operationSpecifiqueDeDiffusion(); // Diffusion atomique, sequentiel et epoque suivant la strategie. 
+	 * }
+	 * 
+	 */
 
 	public void execute() {
 		sensor.updateObservers();
